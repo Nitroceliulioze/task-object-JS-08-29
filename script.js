db6837277de98968c04bc0fd9c3c529667ab62b3
Your NameNitroceliulioze <aksionova.diana@gmail.com>
2022-08-30 17:27:38 +0300

    add set tasks

--- script.js
+++ script.js
@@ -343,3 +343,103 @@ function myFunction(date) {
     const nextQuarter = closestQuarter.getTime() < date.getTime() ? new Date(closestQuarter.getTime() + quarter) : closestQuarter;
     return nextQuarter.getMinutes();
     }
+
+
+    
+    
+  // SETS 
+
+
+  // Write a function that takes a Set and a value as arguments
+// Check if the value is present in the Set
+
+
+  function myFunction(set, val) {
+    return set.has(val);
+  }
+
+// Write a function that takes a Set as argument
+// Convert the Set to an Array
+// Return the Array
+
+  function myFunction(set) {
+    return [...set];
+  }
+
+  // Write a function that takes two Sets as arguments
+// Create the union of the two sets
+// Return the result
+// Tipp: try not to switch to Arrays, this would slow down your code
+
+function myFunction(a, b) {
+  const union = new Set(a);
+  for (const el of b) {
+  union.add(el)
+  }return union
+}
+
+//tas pats:
+function myFunction(a, b) {
+  const result = new Set(a);
+  b.forEach((el) => result.add(el));
+  return result;
+}
+
+// Write a function that takes three elements of any type as arguments
+// Create a Set from those elements
+// Return the result
+
+function myFunction(a, b, c) {
+   return new Set([a, b, c])
+}
+
+//tas pats:
+function myFunction(a, b, c) {
+  const set = new Set();
+  set.add(a);
+  set.add(b);
+  set.add(c);
+  return set;
+}
+
+// Write a function that takes a Set and a value as argument
+// If existing in the Set, remove the value from the Set
+// Return the result
+
+function myFunction(set, val) {
+  set.delete(val)
+  return set
+}
+
+// Write a function that takes a Set and an array as arguments
+// If not already existing, add each element in the array to the Set
+// Return the modified Set
+
+function myFunction(set, arr) {   
+  return new Set([...set,...arr])
+}
+
+//tas pats: 
+function myFunction(set, arr) {
+  arr.forEach((e) => set.add(e));
+  return set;
+}
+
+// Write a function that takes two sets (a and b) as arguments
+// Get the intersection of the sets
+// In other words, return a set containing all elements that are both in a as well as b
+
+
+function myFunction(a, b) {
+  const inter = new Set(
+  [...a].filter(el => b.has(el))
+  );
+    return inter
+}
+
+//tas pats: 
+function myFunction(a, b) {
+  const int = new Set();
+  a.forEach(el => b.has(el) && int.add(el));
+  return int;
+}
